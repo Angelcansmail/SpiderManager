fbdce20ca8611f456ca03e53d67b48b3095b972f
Angelcansmail <1845973547@qq.com>
2018-07-30 13:31:16 +0800

    update

---
+++ commit.sh
@@ -0,0 +1,20 @@
+#!/system/bin/sh
+
+#================================================================
+#   Copyright (C) 2018 Sangfor Ltd. All rights reserved.
+#   
+#   文件名称：commit.sh
+#   创 建 者：Gangzh
+#   创建日期：2018年07月30日
+#   描    述：
+#
+#================================================================
+
+commit=$1
+
+rm */*.pyc
+rm */*/*.pyc
+rm */*/*/*.pyc
+git add *
+git commit -m $commit
+git push origin master
