45f4396267b7d88f6370f895bbed68494d1db8c0
Angelcansmile <1845973547@qq.com>
2018-10-09 19:27:59 +0800

    delete script/*json

--- script/mysql-elasticsearch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-bin=${JDBC_IMPORTER_HOME}/bin
-lib=${JDBC_IMPORTER_HOME}/lib
-
-java \
-    -cp "${lib}/*" \
-    -Dlog4j.configurationFile=${bin}/log4j2.xml \
-    org.xbib.tools.Runner \
-    org.xbib.tools.JDBCImporter $1
--- script/sqldata/usually.sql
+++
@@ -1,4 +0,0 @@
--- 导出数据库
-mysqldump -uroot -p datap> Dump20181009.sql
--- 关联ip和port表
-select a.ip,port,hostname,name,product,version,hackinfo,timesearch from snifferdata a join ip_maindata b on (a.ip=b.ip and hackinfo like'%hole%');
